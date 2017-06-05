7e23c8e41b56eb213aa45ebfcf07b1688578ef31
Sandro Galfo <h0r41i0@hotmail.com>
2017-06-05 12:13:27 +0200

    Added test project incl. xunit references.

--- Gallery.Common.Tests/Properties/AssemblyInfo.cs
+++ Gallery.Common.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Gallery.Common")]
+[assembly: AssemblyTitle("Gallery.Common.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gallery.Common")]
+[assembly: AssemblyProduct("Gallery.Common.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8e4e9303-c6e3-4f46-8075-92c02b5c8ecf")]
+[assembly: Guid("73493444-9b1b-45d1-84c9-ed9378075641")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("Gallery.Common.Tests")]
