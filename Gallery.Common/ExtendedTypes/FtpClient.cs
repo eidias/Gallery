54dad7c59a5e443a2d8d31629d0b64d6226ba083
Sandro Galfo <h0r41i0@hotmail.com>
2017-06-06 11:19:44 +0200

    New FtpClient.

---
+++ Gallery.Common/ExtendedTypes/FtpClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Common.ExtendedTypes
+{
+    //WebClient descends from System.ComponentModel.Component, which is defined with a [DesignerCategory("Component")] attribute.
+    //Decorating our derived class prevents VS from opening it with a designer.
+    [DesignerCategory("Code")]
+    public class FtpClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            FtpWebRequest ftpWebRequest = base.GetWebRequest(address) as FtpWebRequest;
+            ftpWebRequest.EnableSsl = true;
+            return ftpWebRequest;
+        }
+    }
+}
