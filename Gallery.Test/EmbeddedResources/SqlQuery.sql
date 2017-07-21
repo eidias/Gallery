ed7dcc99ae401958f5e2df221f7b4ca93273e242
Sandro Galfo <h0r41i0@hotmail.com>
2017-07-21 17:36:04 +0200

    Added test project including helper method.

---
+++ Gallery.Test/EmbeddedResources/SqlQuery.sql
@@ -0,0 +1,7 @@
+﻿--<Declaration>--
+DECLARE @p0 VARCHAR(32) = 'Gallery'
+--</Declaration>--
+
+--<SelectFullTextSample>--
+SELECT [SelectFullTextSampleId] FROM [dbo].[SelectFullTextSample] WHERE CONTAINS(*, @p0)
+--</SelectFullTextSample>--
