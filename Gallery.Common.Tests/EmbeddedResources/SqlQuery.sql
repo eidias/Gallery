b902dac2850574c7b132bfdb6aaf93fb2c2cedaa
Sandro Galfo <h0r41i0@hotmail.com>
2017-07-21 17:37:54 +0200

    Cleanup.

--- Gallery.Common.Tests/EmbeddedResources/SqlQuery.sql
+++
@@ -1,7 +0,0 @@
-﻿--<Declaration>--
-DECLARE @p0 VARCHAR(32) = 'Gallery'
---</Declaration>--
-
---<SelectFullTextSample>--
-SELECT [SelectFullTextSampleId] FROM [dbo].[SelectFullTextSample] WHERE CONTAINS(*, @p0)
---</SelectFullTextSample>--
