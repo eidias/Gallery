150d3a8725f82e620ad44c81355af23ef1e22511
Sandro Galfo <h0r41i0@hotmail.com>
2017-11-20 16:20:04 +0100

    Added overload to create a mocked DbSet.

--- Gallery.Library.Tests/Helpers/MoqHelper.cs
+++ Gallery.Library.Tests/Helpers/MoqHelper.cs
@@ -19,6 +19,25 @@ namespace Gallery.Library.Helpers
             mockDbSet.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
             return mockDbSet;
         }
+        
+        internal static Mock<DbSet<TEntity>> MockDbSet<TEntity>(ICollection<TEntity> collection) where TEntity : class
+        {
+            //Generic placeholder for an entity
+            TEntity entity = It.IsAny<TEntity>();
 
+            //Mocked ICollection methods
+            Mock<DbSet<TEntity>> mockDbSet = new Mock<DbSet<TEntity>>();
+            mockDbSet.Setup(x => x.Add(entity)).Callback<TEntity>(x => collection.Add(x));
+            mockDbSet.Setup(x => x.Remove(entity)).Callback<TEntity>(x => collection.Remove(x));
+
+            //Mocked IQueryable implementation
+            IQueryable<TEntity> queryable = collection.AsQueryable();
+            mockDbSet.As<IQueryable<TEntity>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            mockDbSet.As<IQueryable<TEntity>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockDbSet.As<IQueryable<TEntity>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockDbSet.As<IQueryable<TEntity>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+
+            return mockDbSet;
+        }
     }
 }
