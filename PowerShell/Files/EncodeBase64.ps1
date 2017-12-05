3029fa801ac26a7b1347e49e599f101462f3b491
Sandro Galfo <h0r41i0@hotmail.com>
2017-12-05 22:09:49 +0100

    New script for handling base64 encoding of files.

---
+++ PowerShell/Files/EncodeBase64.ps1
@@ -0,0 +1,21 @@
+﻿#
+# EncodeBase64.ps1
+#
+# Encode or decode a file to/from a base64 string
+Exit
+
+function Encode-ToBase64
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        $fileItem
+    )
+    process
+    {
+        $bytes = Get-Content -Encoding Byte -Path $fileItem.FullName
+        return [Convert]::ToBase64String($bytes)
+   }
+}
+Get-ChildItem C:\Temp -Filter *.dll -Recurse | Encode-ToBase64
+
