2c7effd17691a2099b7e01d037424f171d198055
Sandro Galfo <h0r41i0@hotmail.com>
2018-01-03 12:03:33 +0100

    Added new monitoring commands.

---
+++ PowerShell/Windows/Monitoring.ps1
@@ -0,0 +1,14 @@
+﻿#
+# Monitoring.ps1
+#
+# Monitoring clients and servers for various parameters
+Exit
+
+
+# Get the current CPU load
+Get-WmiObject -Class win32_processor | Measure-Object -Property LoadPercentage -Average | Select-Object -ExpandProperty Average | Write-Host
+
+
+# Get the current memory load
+$operatingsystem = Get-WmiObject -Class win32_operatingsystem
+($operatingsystem.TotalVisibleMemorySize - $operatingsystem.FreePhysicalMemory) / $operatingsystem.TotalVisibleMemorySize * 100 | Write-Host
