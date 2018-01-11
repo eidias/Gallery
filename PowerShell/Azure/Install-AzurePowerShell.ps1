8a15f93459722dea9a91cb9c87d27ade2532d8ea
Sandro Galfo <h0r41i0@hotmail.com>
2018-01-11 22:36:20 +0100

    Added script for installing Azure PowerShell commandlets.

---
+++ PowerShell/Azure/Install-AzurePowerShell.ps1
@@ -0,0 +1,9 @@
+﻿# Installs the Azure PowerShell module from the PowerShell Gallery
+# PowerShell Gallery is by default un untrusted source and needs to be trused for the Azure commands to work
+# Needs to be run from an elevated session, but only once when the module is installed
+# If the NuGet package provider is outdated, you will be asked to allow updating before the module is installed
+
+
+# Clobbering allows importing modules having command names that already exists in other modules. The last loaded module wins.
+Install-Module AzureRM -AllowClobber
+
