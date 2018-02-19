92a160f92fe7e7d1bce2ed21309ffb407ddf85d4
Sandro Galfo <h0r41i0@hotmail.com>
2018-02-19 21:04:05 +0100

    Added XmlComposer class including SerializeWithEnumerator method.

---
+++ Gallery.Common/Xml/XmlComposer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Gallery.Common.Xml
+{
+    public class XmlComposer
+    {
+        public static void SerializeWithEnumerator<TItem>(IEnumerable<TItem> items, XElement rootElement, Stream stream, Action<TItem> onSerializing = null)
+        {
+            var xDocument = new XDocument();
+            xDocument.Add(rootElement);
+
+            var enumerator = items.GetEnumerator();
+            //Sets the enumerator to its initial position, which is before the first element in the collection.
+            enumerator.Reset();
+
+            var xmlSerializer = new XmlSerializer(typeof(TItem));
+            var xmlSerializerNamespaces = new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty });
+
+            using (var xmlWriter = rootElement.CreateWriter())
+            {
+                while (enumerator.MoveNext())
+                {
+                    onSerializing?.Invoke(enumerator.Current);
+
+                    xmlWriter.WriteWhitespace(string.Empty);
+                    xmlSerializer.Serialize(xmlWriter, enumerator.Current, xmlSerializerNamespaces);
+                }
+            }
+            xDocument.Save(stream);
+        }
+    }
+}
