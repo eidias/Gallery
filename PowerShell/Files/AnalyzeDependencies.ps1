b362b62c93f3a7fbe2304c513634c81a02f5431c
Sandro Galfo <h0r41i0@hotmail.com>
2018-10-18 12:43:26 +0200

    Added dependency analyzer.

---
+++ PowerShell/Files/AnalyzeDependencies.ps1
@@ -0,0 +1,20 @@
+﻿#
+# AnalyzeDependencies.ps1
+#
+# Check assembly versions and dependencies
+
+
+# List DLLs with all their dependencies
+$dependencies = @()
+Get-ChildItem -Path "C:\Users\h0r41\Documents\Visual Studio 2017\Projects\Acme\Acme.Web\bin" -File -Filter *.dll -Recurse | % {
+    $assembly  = [Reflection.Assembly]::LoadFile($_.FullName)
+    $assembly.GetReferencedAssemblies() | % {
+        $dependency = @{}
+        $dependency.Assembly = $assembly.FullName
+        $dependency.Reference = $_.Name
+        $dependency.Version = $_.Version
+        $dependencies += New-Object -TypeName PSObject -Prop $dependency
+
+    }
+}
+$dependencies | Out-GridView
