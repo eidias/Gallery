106f1a17c85595d71821b63304c7d47ec3ea39be
Sandro Galfo <h0r41i0@hotmail.com>
2018-12-03 23:15:36 +0100

    Improved version of Monitoring script.

--- PowerShell/Windows/Monitoring.ps1
+++ PowerShell/Windows/Monitoring.ps1
@@ -12,3 +12,18 @@ Get-WmiObject -Class win32_processor | Measure-Object -Property LoadPercentage -
 # Get the current memory load
 $operatingsystem = Get-WmiObject -Class win32_operatingsystem
 ($operatingsystem.TotalVisibleMemorySize - $operatingsystem.FreePhysicalMemory) / $operatingsystem.TotalVisibleMemorySize * 100 | Write-Host
+
+
+# Show last 10 errors
+Get-EventLog -LogName System -EntryType Error -Newest 10 | Select-Object -ExpandProperty Message | Write-Host
+
+
+#IIS
+Import-Module WebAdministration
+Get-ChildItem -Path IIS:\Sites
+
+
+# Windows update
+Get-WULastInstallationDate
+Start-WUScan
+Install-WUUpdates
